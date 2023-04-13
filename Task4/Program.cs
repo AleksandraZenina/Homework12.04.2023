caacd274d948b119992447d4ae8a9b39f942c224
Aleksandra Zenina <aleksandra248620@yandex.ru>
2023-04-13 23:10:44 +0500

    Task4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу. которая на вход принимает число N, а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+if (N>0)
+{
+    for(int i = 2; i<=N; i+=2)
+    {
+        Console.Write($"{i} ");
+    }
+}
+else Console.WriteLine ("Вы ввели неверное число");
